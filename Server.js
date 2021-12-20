ff0c99ddc2e431bafc6e3d4ac246cdf2d3e6f7be
thomas1y <thomasyohannes15@gmail.com>
2021-12-20 14:39:03 -0800

    make it better

--- Server.js
+++ Server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+const dotenv=require('dotenv');
 
 const app = express();
 
-
+dotenv.config()
 // Connect Database
 connectDB();
 
