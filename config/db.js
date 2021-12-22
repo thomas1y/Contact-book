1e82af1de70496c942ef1d2688a3bd72896c6da3
thomas1y <thomasyohannes15@gmail.com>
2021-12-21 23:59:40 -0800

    make it better

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-//const config = require('config');
-//const db = config.get("mongoURI");
+const config = require('config');
+const db = config.get("MONGO_URI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       //useCreateIndex: true,
       //useFindAndModify: false,
